cd9c07ac9a32b54f55a6bc0647c008d260cfc578
Evie Dickman <GeneralEvie9@outlook.com>
2024-07-19 09:21:28 -0500

    update

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
     <div className='about'>
         <img className='profilePic' src={ProfilePic} alt='' />
         <h1>Hi I'm Bret</h1>
-        <p className='bio'>I'm a web developer who loves working on front-end projects. I'm skilled in HTML, CSS, and JavaScript, and I focus on building responsive and user-friendly interfaces. I have experience with React and TypeScript, which I use to create dynamic web applications. I'm a strong communicator and problem-solver, always eager to learn new technologies. I'm excited to bring my expertise to a dynamic development team and contribute to their success.</p>
+        <p className='bio'>I'm a self taught web developer who loves working on front-end projects. I'm skilled in HTML, CSS, and JavaScript, and I focus on building responsive and user-friendly interfaces. I have experience with React and TypeScript, which I use to create dynamic web applications. I'm a strong communicator and problem-solver, always eager to learn new technologies. I'm excited to bring my expertise to a dynamic development team and contribute to their success.</p>
     </div> 
   )
 }
