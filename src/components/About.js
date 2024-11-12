0f3cde1551d92f2954f9f608b3c869264275067c
Evie Dickman <GeneralEvie9@outlook.com>
2024-11-12 13:53:10 -0600

    evie

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
   return (
     <div className='about'>
-        <h1>Hi I'm Bret</h1>
+        <h1>Hi I'm Evie</h1>
         <p className='bio'>I'm a self taught web developer who loves working on front-end projects. I'm skilled in HTML, CSS, and JavaScript, and I focus on building responsive and user-friendly interfaces. I have experience with React and TypeScript, which I use to create dynamic web applications. I'm a strong communicator and problem-solver, always eager to learn new technologies. I'm excited to bring my expertise to a dynamic development team and contribute to their success.</p>
     </div> 
   )
